0550a76ecfdb182644c85f57c28d182020396e27
gianmarco-tucceri-cimini <g.tuccericimini@icloud.com>
2023-01-05 16:07:10 +0100

    Like button added

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
-from flask import Flask, render_template
+
+
+from flask import Flask, render_template, request, make_response
 import pandas as pd
 import datetime
 from datetime import date
+import logging
+import uuid
+import hashlib
 
 app = Flask(__name__)
+
+def generate_unique_id():
+    # Recupera informazioni univoche dal dispositivo dell'utente
+    user_agent = request.headers.get('User-Agent')
+    user_ip = request.remote_addr
+
+    # Crea una stringa con queste informazioni
+    user_info = user_agent + user_ip
+
+    # Calcola l'hash della stringa
+    user_hash = hashlib.sha256(user_info.encode()).hexdigest()
+
+    # Restituisci l'hash come identificatore univoco dell'utente
+    return user_hash
+
+def get_current_user():
+    # Controlla se esiste già un cookie per l'utente
+    user_id = request.cookies.get("user_id")
+
+    # Se non esiste, genera un nuovo ID e lo salva come cookie
+    if user_id is None:
+        user_id = generate_unique_id()
+        response = make_response(render_template("index.html"))
+        response.set_cookie("user_id", user_id)
+        return user_id
+    # Se esiste già un cookie, restituisci l'ID senza modificare il browser dell'utente
+    else:
+        return user_id, None
 
 @app.route("/")
 def index():
@@ -25,6 +58,29 @@ def index():
         return render_template("index.html", message="il giornale lo scaricano alle 9")
 
     return render_template("index.html", articles=articles)
+
+@app.route("/like", methods=["POST"])
+def like():
+    # Carica il dataframe delle interazioni
+    df_interactions = pd.read_csv("data/interactions.csv")
+
+    # Recupera l'utente corrente (per esempio, dal browser o dal dispositivo)
+    current_user = get_current_user()
+
+    # Recupera il link dell'articolo dai dati inviati con la richiesta POST
+    link = request.form["link"]
+
+    # Controlla se l'utente ha già espresso un like per questo articolo
+    already_liked = (df_interactions['user_id'] == current_user) & (df_interactions['link'] == link)
+
+    # Se l'utente non ha ancora espresso un like per questo articolo, aggiungi una nuova riga al dataframe
+    if not already_liked.any():
+        df_interactions = df_interactions.append({'user_id': current_user, 'link': link}, ignore_index=True)
+
+    # Salva il dataframe aggiornato
+    df_interactions.to_csv("data/interactions.csv", index=False)
+
+    return "OK"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
